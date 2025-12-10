1d1ee1bf7fbab143cba49bd05d890eedd3f4c37f
Nicholas Sorokin <nicholas.sorokin@hotmail.com>
2025-12-10 11:31:02 +1030

    fix: give baddie a sprite

--- src/ollie-bird/game-object/Baddie.ts
+++ src/ollie-bird/game-object/Baddie.ts
@@ -4,11 +4,24 @@ import type IGame from '../IGame';
 import RectangleCollider from '../RectangleCollider';
 import Obstacle from './Obstacle';
 
+import baddie1 from '../../assets/baddie-1.png';
+import baddie2 from '../../assets/baddie-2.png';
+
 export default class Baddie extends GameObject {
 	layer = GameObject.LAYER_ENEMYS;
 	dir = Math.sign(Math.random() - 0.5) || 1;
 	time = 0;
 	speed = 2;
+
+	static sprites = {
+		baddie1: new Image(),
+		baddie2: new Image(),
+	};
+
+	static {
+		Baddie.sprites.baddie1.src = baddie1;
+		Baddie.sprites.baddie2.src = baddie2;
+	}
 
 	constructor(game: IGame, public x: number, public y: number) {
 		super(game);
@@ -55,6 +68,19 @@ export default class Baddie extends GameObject {
 			this.y + GRID_SIZE / 2,
 			GRID_SIZE,
 			GRID_SIZE / 2,
+		);
+
+		const sprite =
+			this.time % 20 < 10
+				? Baddie.sprites.baddie1
+				: Baddie.sprites.baddie2;
+
+		context.drawImage(
+			sprite,
+			this.x - GRID_SIZE * 0.2,
+			this.y - GRID_SIZE * 0.2 + GRID_SIZE / 2,
+			GRID_SIZE * 1.4,
+			(1.4 * GRID_SIZE) / 2,
 		);
 	}
 }
