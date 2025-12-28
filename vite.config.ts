ab515626d3d6227d6d626d8cb06fb21c47169023
Nicholas Sorokin <nicholas.sorokin@hotmail.com>
2025-12-28 12:15:09 +1030

    feat: add decorator support

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react({
+			babel: {
+				plugins: [
+					[
+						'@babel/plugin-proposal-decorators',
+						{ version: '2023-11' },
+					],
+				],
+			},
+		}),
+	],
 	base: process.env.VITE_BASE_URL || '/',
 });
