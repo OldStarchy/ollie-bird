b1ae2aec43cc37d8409a11bff2685f89be947edf
Nicholas Sorokin <nicholas.sorokin@hotmail.com>
2026-01-03 01:39:52 +1030

    fix: addr. review comments

--- src/components/PropertiesPanel.tsx
+++ src/components/PropertiesPanel.tsx
@@ -1,9 +1,7 @@
-import { useCallback, useEffect, useId, useMemo, useState } from 'react';
-import z from 'zod';
+import { useEffect, useMemo, useState } from 'react';
 import getPropertiesSchema from '../property/getPropertiesSchema';
 import type { NotifyPropertyChanged } from '../property/NotifyPropertyChanged';
-import Input from './Input';
-import Select from './Select';
+import ZodField from './ZodField';
 
 export default function PropertiesPanel<T extends NotifyPropertyChanged>({
 	model,
@@ -41,109 +39,6 @@ export default function PropertiesPanel<T extends NotifyPropertyChanged>({
 					}}
 				/>
 			))}
-		</div>
-	);
-}
-
-function ZodField({
-	schema,
-	value,
-	onChange: doChange,
-}: {
-	schema: z.ZodType;
-	value: any;
-	onChange: (v: any) => void;
-}) {
-	const [enteredValue, setEnteredValue] = useState(value);
-	const [warning, setWarning] = useState<string>();
-	const id = useId();
-
-	useEffect(() => {
-		setEnteredValue(value);
-	}, [value]);
-
-	const onChange = useCallback(
-		(newValue: unknown) => {
-			setEnteredValue(newValue);
-			const result = schema.safeParse(newValue);
-
-			if (result.success) {
-				doChange(result.data);
-				setWarning(undefined);
-			} else {
-				setWarning(z.prettifyError(result.error));
-			}
-		},
-		[schema, doChange],
-	);
-
-	const onBlur = useCallback(() => {
-		setEnteredValue(value);
-	}, [value]);
-
-	return (
-		<div>
-			<label htmlFor={id}>{schema.description}</label>
-			{(() => {
-				switch (schema.type) {
-					case 'string':
-						return (
-							<Input
-								id={id}
-								type="text"
-								value={enteredValue}
-								onChange={(e) => onChange(e.target.value)}
-								onBlur={onBlur}
-							/>
-						);
-					case 'number':
-						return (
-							<Input
-								id={id}
-								type="number"
-								value={enteredValue}
-								onChange={(e) =>
-									onChange(Number(e.target.value))
-								}
-								onBlur={onBlur}
-							/>
-						);
-					case 'enum': {
-						const options = Object.entries(
-							(schema as z.ZodEnum<any>).def.entries,
-						);
-						return (
-							<Select
-								id={id}
-								value={enteredValue}
-								onChange={(e) => {
-									onChange(e.target.value);
-									onBlur();
-								}}
-							>
-								{options.map(([k, _v]: [string, unknown]) => (
-									<option key={k} value={k}>
-										{k}
-									</option>
-								))}
-							</Select>
-						);
-					}
-					case 'boolean':
-						return (
-							<Input
-								id={id}
-								type="checkbox"
-								checked={value}
-								onChange={(e) => onChange(e.target.checked)}
-								onBlur={onBlur}
-							/>
-						);
-					default:
-						return <pre>Unsupported type: {schema.type}</pre>;
-				}
-			})()}
-			{warning ?? <label>{warning}</label>}
 		</div>
 	);
 }
