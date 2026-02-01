249f140b80797b753a6161d8952e7273ae8d4782
Nicholas Sorokin <nicholas.sorokin@hotmail.com>
2026-02-01 17:03:48 +1030

    fix: test env missing disposable stack

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
 	test: {
 		environment: 'happy-dom',
+		setupFiles: ['disposablestack/auto', './src/rxjs-disposable'],
 		coverage: {
 			provider: 'v8',
 			reporter: ['json', 'json-summary'],
