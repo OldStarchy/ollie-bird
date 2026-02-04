3db35919fc845f2a430d6be00bdf61de9ab0281d
Nicholas Sorokin <nicholas.sorokin@hotmail.com>
2026-02-04 20:47:09 +1030

    feat: add player 2 controller bindings

--- src/ollie-bird/OllieBirdGame.ts
+++ src/ollie-bird/OllieBirdGame.ts
@@ -18,55 +18,94 @@ class OllieBirdGame extends BaseGame {
 	constructor() {
 		super();
 
-		const xLeft = this.input.gamepads.getAxisButton(
-			0,
-			XboxGamepadAxisMap.LeftStickX,
-			0.3,
-			true,
-		);
-		const xRight = this.input.gamepads.getAxisButton(
-			0,
-			XboxGamepadAxisMap.LeftStickX,
-			0.3,
-		);
-		const gamepadA = this.input.gamepads.getButton(
-			0,
-			XboxGamepadButtonMap.A,
-		);
-		const gamepadX = this.input.gamepads.getButton(
-			0,
-			XboxGamepadButtonMap.X,
-		);
-		const dPadLeft = this.input.gamepads.getButton(
-			0,
-			XboxGamepadButtonMap.DPadLeft,
-		);
-		const dPadRight = this.input.gamepads.getButton(
-			0,
-			XboxGamepadButtonMap.DPadRight,
-		);
+		{
+			const xLeft = this.input.gamepads.getAxisButton(
+				0,
+				XboxGamepadAxisMap.LeftStickX,
+				0.3,
+				true,
+			);
+			const xRight = this.input.gamepads.getAxisButton(
+				0,
+				XboxGamepadAxisMap.LeftStickX,
+				0.3,
+			);
+			const gamepadA = this.input.gamepads.getButton(
+				0,
+				XboxGamepadButtonMap.A,
+			);
+			const gamepadX = this.input.gamepads.getButton(
+				0,
+				XboxGamepadButtonMap.X,
+			);
+			const dPadLeft = this.input.gamepads.getButton(
+				0,
+				XboxGamepadButtonMap.DPadLeft,
+			);
+			const dPadRight = this.input.gamepads.getButton(
+				0,
+				XboxGamepadButtonMap.DPadRight,
+			);
 
-		const keyLeft = this.input.keyboard.getButton('ArrowLeft');
-		const keyRight = this.input.keyboard.getButton('ArrowRight');
-		const keyUp = this.input.keyboard.getButton('ArrowUp');
-		const keyR = this.input.keyboard.getButton('KeyR');
+			const keyLeft = this.input.keyboard.getButton('ArrowLeft');
+			const keyRight = this.input.keyboard.getButton('ArrowRight');
+			const keyUp = this.input.keyboard.getButton('ArrowUp');
+			const keyR = this.input.keyboard.getButton('KeyR');
 
-		const birdControls: BirdControls = {
-			Flap: this.input.anyButton([gamepadA, keyUp]),
-			Left: this.input.anyButton([xLeft, keyLeft, dPadLeft]),
-			Right: this.input.anyButton([xRight, keyRight, dPadRight]),
-			Restart: this.input.anyButton([gamepadX, keyR]),
-		};
+			const birdControls: BirdControls = {
+				Flap: this.input.anyButton([gamepadA, keyUp]),
+				Left: this.input.anyButton([xLeft, keyLeft, dPadLeft]),
+				Right: this.input.anyButton([xRight, keyRight, dPadRight]),
+				Restart: this.input.anyButton([gamepadX, keyR]),
+			};
 
-		this.input.defineSchema<BirdControls>('Player 1', birdControls);
-		this.input.defineSchema<BirdControls>('Player 2', {
-			Flap: this.input.keyboard.getButton('KeyW'),
-			Left: this.input.keyboard.getButton('KeyA'),
-			Right: this.input.keyboard.getButton('KeyD'),
-			Restart: this.input.keyboard.getButton('KeyR'),
-		});
+			this.input.defineSchema<BirdControls>('Player 1', birdControls);
+			this.input.defineButton(Bindings.Restart, birdControls.Restart);
+		}
 
-		this.input.defineButton(Bindings.Restart, birdControls.Restart);
+		{
+			const xLeft = this.input.gamepads.getAxisButton(
+				1,
+				XboxGamepadAxisMap.LeftStickX,
+				0.3,
+				true,
+			);
+			const xRight = this.input.gamepads.getAxisButton(
+				1,
+				XboxGamepadAxisMap.LeftStickX,
+				0.3,
+			);
+			const gamepadA = this.input.gamepads.getButton(
+				1,
+				XboxGamepadButtonMap.A,
+			);
+			const gamepadX = this.input.gamepads.getButton(
+				1,
+				XboxGamepadButtonMap.X,
+			);
+			const dPadLeft = this.input.gamepads.getButton(
+				1,
+				XboxGamepadButtonMap.DPadLeft,
+			);
+			const dPadRight = this.input.gamepads.getButton(
+				1,
+				XboxGamepadButtonMap.DPadRight,
+			);
+
+			const keyUp = this.input.keyboard.getButton('KeyW');
+			const keyLeft = this.input.keyboard.getButton('KeyA');
+			const keyRight = this.input.keyboard.getButton('KeyD');
+			const keyR = this.input.keyboard.getButton('KeyR');
+
+			const birdControls: BirdControls = {
+				Flap: this.input.anyButton([gamepadA, keyUp]),
+				Left: this.input.anyButton([xLeft, keyLeft, dPadLeft]),
+				Right: this.input.anyButton([xRight, keyRight, dPadRight]),
+				Restart: this.input.anyButton([gamepadX, keyR]),
+			};
+
+			this.input.defineSchema<BirdControls>('Player 2', birdControls);
+		}
 	}
 
 	override preStart(): void {
