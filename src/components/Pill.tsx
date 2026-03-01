18ebbbdeea4eeb4fd38dc19756c7acf7501dced1
Nicholas Sorokin <nicholas.sorokin@hotmail.com>
2026-03-01 14:02:40 +1030

    feat: add Pill component for colored words

---
+++ src/components/Pill.tsx
@@ -0,0 +1,11 @@
+import { styled } from '@stitches/react';
+
+const Pill = styled('code', {
+	fontFamily: 'monospace',
+	fontSize: '0.9em',
+	backgroundColor: 'var(--color-code-bg)',
+	padding: '0.1em 0.3em',
+	borderRadius: '3px',
+});
+
+export default Pill;
