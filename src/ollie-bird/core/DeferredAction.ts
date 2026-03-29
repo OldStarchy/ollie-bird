cfff4372e73eb9f252a7b8c3c40cf8e144810c10
Nicholas Sorokin <nicholas.sorokin@hotmail.com>
2026-03-29 23:24:09 +1030

    docs: add more doc comments

--- src/ollie-bird/core/DeferredAction.ts
+++ src/ollie-bird/core/DeferredAction.ts
@@ -1,3 +1,33 @@
+/**
+ * Allows blocking an action from occuring for some time.
+ *
+ * Very much the same as a rising-edge debounce, but
+ * * can be invoked immediately if there are no blockers
+ *   ```ts
+ *   const notifyFinished = new DeferredAction(() => console.log('Finished!'));
+ *
+ *   notifyFinished.invoke(); // prints "Finished!" immediately
+ *   ```
+ *
+ * * defers the action until blockers are disposed
+ *   ```ts
+ *   async function doSomething() {
+ *     using _blocker = notifyFinished.defer();
+ *
+ *     await new Promise(resolve => setTimeout(resolve, 1000));
+ *   }
+ *
+ *   void doSomething();
+ *   notifyFinished.invoke(); // does nothing, the action is blocked
+ *
+ *   // After 1 second, "Finished!" is logged to the console.
+ *   ```
+ *
+ * * the action only executes if it was requested during the time it was blocked
+ *   ```ts
+ *   await doSomething(); // prints nothing, invoke was not called
+ *   ```
+ */
 export default class DeferredAction {
 	#action: () => void;
 	#count: number = 0;
@@ -7,6 +37,10 @@ export default class DeferredAction {
 		this.#action = action;
 	}
 
+	/**
+	 * Prevents this action from executing at least until the returned
+	 * `Disposable` is disposed.
+	 */
 	defer(): Disposable {
 		this.#count++;
 		return {
@@ -17,11 +51,20 @@ export default class DeferredAction {
 		};
 	}
 
+	/**
+	 * Requests that this action be executed as soon as possible. If there are
+	 * no blockers, the action is executed immediately. Otherwise, the action is
+	 * executed as soon as all blockers are removed.
+	 */
 	invoke(): void {
 		this.#shouldExecute = true;
 		this.#maybeExecute();
 	}
 
+	/**
+	 * A convenience method that calls both {@link defer} and {@link invoke} in
+	 * one step.
+	 */
 	deferInvoke(): Disposable {
 		const disposable = this.defer();
 		this.invoke();
--- src/ollie-bird/core/GameObject.ts
+++ src/ollie-bird/core/GameObject.ts
@@ -3,6 +3,7 @@ import z from 'zod';
 import onChange from '../../react-interop/onChange';
 import { ReactInterop } from '../../react-interop/ReactInterop';
 import { TAG_LEVEL_STRUCTURE } from '../const';
+import type _BaseGame from './BaseGame';
 import type IGame from './IGame';
 import type IModular from './IModular';
 import Module from './Module';
@@ -37,118 +38,269 @@ export const gameObjectDtoSchemaV1 = z.object({
 
 export type GameObjectDto = z.input<typeof gameObjectDtoSchemaV1>;
 
+/**
+ * A GameObject is basically a glorified collection of {@link Module | Modules}.
+ */
 export default class GameObject
 	implements IModular, Disposable, ReactInterop<GameObjectView>, Serializable
 {
+	// TypeScript workaround for type-checking static members for the
+	// serializer.
 	declare ['constructor']: Pick<typeof GameObject, keyof typeof GameObject>;
 
-	static readonly defaultName: string = 'Game Object';
+	readonly #disposableStack = new DisposableStack();
+	readonly #modules: ModuleCollection;
 
-	protected disposableStack = new DisposableStack();
-	private modules: ModuleCollection;
 	#initialized = false;
 	get initialized() {
 		return this.#initialized;
 	}
 
+	/**
+	 * The layer of this object. Objects with higher layers are rendered on top
+	 * of objects with lower layers.
+	 *
+	 * Since the usefulness of layers is defined by being relative to (higher or
+	 * lower) other objects, a good practice is to create a set of named layers
+	 * in one place where the order is clear.
+	 *
+	 * This is a good use for TypeScript's enums. They define a set of ordered
+	 * values in a concise way. The actual numeric values do not matter and are
+	 * implicit.
+	 *
+	 * For example:
+	 * ```ts
+	 * export default enum Layer {
+	 *   Background,
+	 *   Terrain,
+	 *   Entities,
+	 *   Player,
+	 *   Foreground,
+	 * }
+	 *
+	 * // Will render above Entities but behind Foreground
+	 * playerObj.layer = Layer.Player;
+	 * ```
+	 */
 	@onChange((self) => self.notify())
 	accessor layer: number = 0;
+
+	/**
+	 * Tags are just strings that categorize objects in whatever way you find
+	 * useful.
+	 *
+	 * Similar to layers, its useful to define a set of tags in one place, and
+	 * assign meaning to those tags.
+	 *
+	 * For example:
+	 * ```ts
+	 * /// Touching an object with this tag will kill the player
+	 * export const TAG_DEADLY = 'deadly';
+	 *
+	 * /// Objects with this tag will not be saved when saving the game.
+	 * export const TAG_TEMPORARY = 'temporary';
+	 * ```
+	 *
+	 * (note that triple-slash comments are used in the example because nested
+	 * doc-comments are invalid)
+	 */
 	@onChange((self) => self.notify())
 	accessor tags: Set<string> = new Set();
+
+	/**
+	 * A nice name for this object. Useful for debugging and editor purposes.
+	 */
 	@onChange((self) => self.notify())
 	accessor name: string;
 
+	/**
+	 * Represents the position~~, rotation and scale of this object~~.
+	 *
+	 * Rotation and scale are not yet implementd.
+	 *
+	 * Every GameObject has a Transform2d by default, and it cannot be removed.
+	 */
 	readonly transform: Transform2d;
+
+	/**
+	 * An immutable identifier for this object.
+	 *
+	 * Not saved when serializing.
+	 *
+	 * Currently only used by the react based editor to track object identities
+	 * across updates.
+	 */
 	readonly id: string = Math.random().toString(16).slice(2);
 
 	#change$ = new Subject<void>();
+
+	/**
+	 * Indicates a change to one of this objects inspecatble properties (name,
+	 * tags, layer). Used to trigger updates in the editor.
+	 *
+	 * This observable completes when this object is destroyed.
+	 */
 	readonly change$ = this.#change$.asObservable();
 
 	private notify(): void {
 		this.#change$.next();
 	}
 
+	/**
+	 * Do not use this directly, instead use {@link _BaseGame.spawn|BaseGame.spawn} or
+	 * {@link _BaseGame.spawnPrefab|BaseGame.spawnPrefab}.
+	 *
+	 * @internal
+	 */
 	constructor(readonly game: IGame) {
-		this.name = this.constructor.defaultName;
-		this.modules = new ModuleCollection(this);
-		this.disposableStack.use(this.modules);
+		this.name = `Game Object`;
+		this.#modules = new ModuleCollection(this);
+		this.#disposableStack.use(this.#modules);
 
 		this.transform = this.addModule(Transform2d);
 		this.transform.transient = true;
 	}
 
+	/**
+	 * Iterates over all modules attached to this object that are instances of
+	 * the given type. For example,
+	 * ```ts
+	 * // Get all modules that are instances of the Health component
+	 * for (const health of obj.getModulesByType(Health)) {
+	 *   console.log(health.current);
+	 * }
+	 * ```
+	 *
+	 * Note that this method will return modules that are subclasses of the
+	 * given type as well. For example, if EnemyHealth extends Health, it will
+	 * be returned by `getModulesByType(Health)`.
+	 *
+	 * ## Note
+	 * Iterators are lazy, so be sure to collect the results before
+	 * the next tick (i.e. with {@link IteratorObject.toArray | .toArray()}).
+	 */
 	getModulesByType<T extends Module>(
 		type: abstract new (owner: GameObject, ...args: any[]) => T,
 	): IteratorObject<T> {
-		return this.modules.getModulesByType(type);
+		return this.#modules.getModulesByType(type);
 	}
+
+	/**
+	 * Returns the first module attached to this object that is an instance of
+	 * the given type, or null if no such module exists. For example,
+	 */
 	getModule<T extends Module>(
 		type: abstract new (owner: GameObject, ...args: any[]) => T,
 	): T | null {
-		return this.modules.getModule(type);
+		return this.#modules.getModule(type);
 	}
+
+	/**
+	 * Adds a module of the given type to this object. The module is initialized
+	 * before addModule returns, so the returned module is ready to use
+	 * immediately.
+	 *
+	 * If the module constructor also adds more modules, the sub-modules will
+	 * be initialized first.
+	 */
 	addModule<
 		Constructor extends new (owner: GameObject, ...args: any[]) => Module,
 	>(
 		type: Constructor,
 		...args: Tail<ConstructorParameters<Constructor>>
 	): InstanceType<Constructor> {
-		return this.modules.addModule(type, ...args);
-	}
-	removeModule(module: Module): void {
-		return this.modules.removeModule(module);
+		return this.#modules.addModule(type, ...args);
 	}
 
+	/**
+	 * Removes a module from this object. The module is removed immediately and
+	 * disposed.
+	 */
+	removeModule(module: Module): void {
+		return this.#modules.removeModule(module);
+	}
+
+	/**
+	 * @internal
+	 */
 	initialize() {
 		if (this.#initialized) return;
 
-		this.modules.initialize();
+		this.#modules.initialize();
 		this.#initialized = true;
 	}
 
+	/**
+	 * @internal
+	 */
 	beforeUpdate(): void {
-		this.modules.beforeUpdate();
+		this.#modules.beforeUpdate();
 	}
 
+	/**
+	 * @internal
+	 */
 	update(): void {
-		this.modules.update();
+		this.#modules.update();
 	}
 
+	/**
+	 * @internal
+	 */
 	afterUpdate(): void {
-		this.modules.afterUpdate();
+		this.#modules.afterUpdate();
 	}
 
+	/**
+	 * @internal
+	 */
 	beforeRender(context: CanvasRenderingContext2D): void {
-		this.modules.beforeRender(context);
+		this.#modules.beforeRender(context);
 	}
 
+	/**
+	 * @internal
+	 */
 	render(context: CanvasRenderingContext2D): void {
-		this.modules.render(context);
+		this.#modules.render(context);
 	}
 
+	/**
+	 * @internal
+	 */
 	afterRender(context: CanvasRenderingContext2D): void {
-		this.modules.afterRender(context);
+		this.#modules.afterRender(context);
 	}
 
+	/**
+	 * @internal
+	 */
 	beforeRenderGizmos(context: CanvasRenderingContext2D): void {
-		this.modules.beforeRenderGizmos(context);
+		this.#modules.beforeRenderGizmos(context);
 	}
 
+	/**
+	 * @internal
+	 */
 	renderGizmos(context: CanvasRenderingContext2D): void {
-		this.modules.renderGizmos(context);
+		this.#modules.renderGizmos(context);
 	}
 
+	/**
+	 * @internal
+	 */
 	afterRenderGizmos(context: CanvasRenderingContext2D): void {
-		this.modules.afterRenderGizmos(context);
+		this.#modules.afterRenderGizmos(context);
 	}
 
-	readonly #destroy$ = new Subject<void>();
-	readonly destroy$ = this.#destroy$.asObservable();
 	[Symbol.dispose]() {
-		this.#destroy$.next();
-		this.disposableStack.dispose();
+		this.#change$.complete();
+		this.#disposableStack.dispose();
 	}
 
+	/**
+	 * Schedules this object for destruction using {@link BaseGame.destroy}.
+	 */
 	destroy() {
 		this.game.destroy(this);
 	}
@@ -162,6 +314,15 @@ export default class GameObject
 		};
 	}
 
+	/**
+	 * Converts this object into a plain data object that can be serialized.
+	 *
+	 * All properties (except {@link id}) and all non-{@link Module.transient|transient} modules are
+	 * serialized.
+	 *
+	 * The object returned by this is suitable for use as a prefab. Therefore,
+	 * objects can be cloned with `this.game.spawnPrefab(obj.serialize())`.
+	 */
 	serialize(): GameObjectDto {
 		return {
 			version: 1,
@@ -169,7 +330,7 @@ export default class GameObject
 			tags: this.tags.values().toArray(),
 			layer: this.layer,
 			transform: this.transform.serialize(),
-			modules: this.modules
+			modules: this.#modules
 				.getModules()
 				.filter((module) => module !== this.transform)
 				.filter((module) => !module.transient)
@@ -200,6 +361,11 @@ export default class GameObject
 		this.notify();
 	}
 
+	/**
+	 * The implementation behind {@link BaseGame.spawnPrefab}.
+	 *
+	 * @internal
+	 */
 	static deserializePartial(
 		obj: unknown,
 		context: { game: IGame },
