9d21ee0291a104f5003575f824b8d57dee114847
Nicholas Sorokin <nicholas.sorokin@hotmail.com>
2026-04-03 16:08:19 +1030

    docs: add doc-comments to filterMap

--- src/ollie-bird/core/rxjs/filterMap.ts
+++ src/ollie-bird/core/rxjs/filterMap.ts
@@ -2,6 +2,20 @@ import { Observable } from 'rxjs';
 
 const Skip = Symbol('Skip');
 
+/**
+ * A combination of filter and map for RxJS Observables. The provided mapper function
+ * can return a value to emit, or the special `filterMap.Skip` symbol to skip emitting
+ * for that value.
+ *
+ * ```ts
+ * import { of } from 'rxjs';
+ * import filterMap from './filterMap';
+ *
+ * of(1, 2, 3, 4).pipe(
+ *   filterMap(x => x % 2 === 0 ? x * 2 : filterMap.Skip)
+ * ).subscribe(console.log); // Logs: 4, 8
+ * ```
+ */
 function filterMap<T, R>(
 	mapper: (value: T) => R | typeof Skip,
 ): (source$: Observable<T>) => Observable<R> {
